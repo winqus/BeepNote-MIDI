8505ce85c7f9f343931eb685ad88b8aa33227919
winqus <67459222+winqus@users.noreply.github.com>
2023-04-18 22:27:14 +0300

    Refactoring

--- midi.js
+++ midi.js
@@ -1,23 +1,26 @@
 let isRegistering = false;
-let registeredNotes = new Set();
+var registeredNotes = new Set();
 let beepSound = null;
+const beepCheckbox = document.getElementById('myCheckbox');
+const registerButton = document.getElementById('registerButton');
+registerButton.addEventListener('click', toggleRegistration);
+const clearRegisterButton = document.getElementById('clearRegisterButton');
+clearRegisterButton.addEventListener('click', clearNoteRegister);
 
 function loadBeepSound(url) {
   return new Promise((resolve, reject) => {
     const audio = new Audio();
-    audio.addEventListener("canplaythrough", () => {
+    audio.addEventListener('canplaythrough', () => {
       resolve(audio);
     });
-    audio.addEventListener("error", reject);
+    audio.addEventListener('error', reject);
     audio.src = url;
   });
 }
 
-async function init() {
-  beepSound = await loadBeepSound("short-beep.wav");
+async function initAudio() {
+  beepSound = await loadBeepSound('short-beep.wav');
 }
-
-init();
 
 function playSound() {
   if (beepSound) {
@@ -31,12 +34,12 @@ navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
 function onMIDISuccess(midiAccess) {
   const input = midiAccess.inputs.values().next().value;
   input.onmidimessage = onMIDIMessage;
-  document.getElementById("midi-message").innerText = "MIDI connected";
+  document.getElementById('midi-message').innerText = 'MIDI connected';
 }
 
 function onMIDIFailure() {
-  console.error("Failed to access MIDI devices");
-  document.getElementById("midi-message").innerText = "MIDI not available";
+  console.error('Failed to access MIDI devices');
+  document.getElementById('midi-message').innerText = 'MIDI not available';
 }
 
 function setRegisteredNotesCookie() {
@@ -46,74 +49,55 @@ function setRegisteredNotesCookie() {
 }
 
 function loadRegisteredNotesCookie() {
-  const cookie = document.cookie
-    .split(";")
-    .find((cookie) => cookie.startsWith("registeredNotes="));
+  const cookie = document.cookie.split(';').find((cookie) => cookie.startsWith('registeredNotes='));
   if (cookie) {
-    const notesJson = cookie.split("=")[1];
-    const notesArray = JSON.parse(notesJson);
+    const notesJson = cookie.split('=')[1];
+    const notesArray = JSON.parse(notesJson).map((note) => Number(note));
     registeredNotes = new Set(notesArray);
     const notes = Array.from(registeredNotes)
       .sort((a, b) => a - b)
       .map((note) => midiToNoteName(note))
-      .join(" ");
-    document.getElementById("registeredNotes").innerHTML = notes;
+      .join(' ');
+    document.getElementById('registeredNotes').innerHTML = notes;
   }
 }
 
 function clearRegisteredNotesCookie() {
-  document.cookie =
-    "registeredNotes=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  document.cookie = 'registeredNotes=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
 }
 
 function toggleRegistration() {
   isRegistering = !isRegistering;
-  const registerButton = document.getElementById("registerButton");
-  registerButton.innerHTML = isRegistering
-    ? "Stop Registration"
-    : "Start Registration";
+  const registerButton = document.getElementById('registerButton');
+  registerButton.innerHTML = isRegistering ? 'Stop Registration' : 'Start Registration';
 }
 
 function addNoteToRegister(noteNumber) {
-  registeredNotes.add(noteNumber);
+  registeredNotes.add(Number(noteNumber));
   const notes = Array.from(registeredNotes)
     .sort((a, b) => a - b)
     .map((note) => midiToNoteName(note))
-    .join(" ");
-  document.getElementById("registeredNotes").innerHTML = notes;
+    .join(' ');
+  document.getElementById('registeredNotes').innerHTML = notes;
   setRegisteredNotesCookie();
 }
 
 function clearNoteRegister() {
   registeredNotes = new Set();
-  document.getElementById("registeredNotes").innerHTML = "";
+  document.getElementById('registeredNotes').innerHTML = '';
   clearRegisteredNotesCookie();
 }
 
 function midiToNoteName(midiNoteNumber) {
   const octave = Math.floor(midiNoteNumber / 12) - 1;
-  const notes = [
-    "C",
-    "C#",
-    "D",
-    "D#",
-    "E",
-    "F",
-    "F#",
-    "G",
-    "G#",
-    "A",
-    "A#",
-    "B",
-  ];
+  const notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
   const note = notes[midiNoteNumber % 12];
   return note + octave;
 }
 
 const activeNotes = new Set();
-
 function onMIDIMessage(event) {
-  const noteNumber = event.data[1];
+  const noteNumber = Number(event.data[1]);
 
   if (event.data[0] === 144) {
     activeNotes.add(noteNumber);
@@ -130,26 +114,31 @@ function onMIDIMessage(event) {
     activeNotes.delete(noteNumber);
   }
 
-  const keys = document.querySelectorAll(".key");
-  keys.forEach((key) => {
-    const keyNote = key.dataset.note;
-    if (
-      activeNotes.has(Number(keyNote)) ||
-      [...activeNotes].some((note) => note % 12 === Number(keyNote) % 12)
-    ) {
-      key.classList.add("highlight");
+  const keyElements = document.querySelectorAll('.key');
+  const getNoteIndex = (noteNumber) => noteNumber % 12;
+  const isSameNote = (note1, note2) => getNoteIndex(Number(note1)) === getNoteIndex(Number(note2));
+  const activeAndRegisterdNotes = [...activeNotes].filter((note) => registeredNotes.has(note));
+
+  keyElements.forEach((keyElement) => {
+    const keyNote = Number(keyElement.dataset.note);
+
+    if ([...activeNotes].some((note) => isSameNote(note, keyNote))) {
+      if (
+        [...activeNotes].filter((note) => isSameNote(note, keyNote)).length ===
+        activeAndRegisterdNotes.filter((note) => isSameNote(note, keyNote)).length
+      ) {
+        keyElement.classList.add('registered-highlight');
+        keyElement.classList.remove('highlight');
+      } else {
+        keyElement.classList.add('highlight');
+        keyElement.classList.remove('registered-highlight');
+      }
     } else {
-      key.classList.remove("highlight");
+      keyElement.classList.remove('registered-highlight');
+      keyElement.classList.remove('highlight');
     }
   });
 }
 
-const beepCheckbox = document.getElementById("myCheckbox");
-
-const registerButton = document.getElementById("registerButton");
-registerButton.addEventListener("click", toggleRegistration);
-
-const clearRegisterButton = document.getElementById("clearRegisterButton");
-clearRegisterButton.addEventListener("click", clearNoteRegister);
-
 window.onload = loadRegisteredNotesCookie;
+initAudio();
